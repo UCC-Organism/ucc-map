7c2f71efb338a8f1a6cd2f5e85eeb5c15915d34d
Marcin Ignac <marcin.ignac@gmail.com>
2014-12-16 05:12:01 +0000

    More room types

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ var Config = {
     { type: 'research' , label: 'Research' , color: '#FF00FF', centerColor: '#FF00FF', edgeColor: '#FF00FF' },
     { type: 'admin'    , label: 'Admin'    , color: '#6666FF', centerColor: '#6666FF', edgeColor: '#6666FF' },
     { type: 'closet'   , label: 'Closet'   , color: '#996600', centerColor: '#996600', edgeColor: '#996600' },
+    { type: 'food'     , label: 'Food'     , color: '#FFAA00', centerColor: '#FFAA00', edgeColor: '#FFAA00' },
+    { type: 'knowledge', label: 'Knowledge', color: '#00DDAA', centerColor: '#00DDAA', edgeColor: '#00DDAA' },
     { type: 'exit'     , label: 'Exit'     , color: '#FF0000', centerColor: '#FF0000', edgeColor: '#FF0000' },
     { type: 'empty'    , label: 'Empty'    , color: '#000000', centerColor: '#000000', edgeColor: '#000000' },
     { type: 'cell'     , label: 'Cell'     , color: '#696E98', centerColor: '#696E98', edgeColor: '#FF00FF' } //green
